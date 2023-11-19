e8ba86812447d7c63b1cce94bc2d09914bf1e204
olakoder <oyebanjiyusufolatunji@gmail.com>
2023-11-19 12:30:06 +0100

    beautification

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ collapse.addEventListener('click', close)
 function hamburger() {
     document.querySelector('.mobile-menu').classList.add('hide')
     document.querySelector('.close').classList.add('hide1')
-    document.querySelector('.navigate').classList.add('navigation')
-    document.querySelector('.mobile-menu').classList.add('change')
+    document.querySelector('.navigation').classList.add('active')
+    document.querySelector('.opaque').classList.add('active')
 }
 
 function close() {
     document.querySelector('.mobile-menu').classList.remove('hide')
     document.querySelector('.close').classList.remove('hide1')
-    document.querySelector('.navigate').classList.remove('navigation')
-    // document.querySelector('.mobile-menu').classList.remove('change')
+    document.querySelector('.navigation').classList.remove('active')
+    document.querySelector('.opaque').classList.remove('active')
 
 }
 
@@ -24,8 +24,7 @@ let drop = document.querySelector('.drop')
 drop.addEventListener('click', show);
 
 function show() {
-    document.querySelector('.service').classList.toggle('hide1')
-    // document.querySelector('.services').classList.toggle('hide1')
+    // document.querySelector('.service').classList.toggle('hide1')
 }
 
 let drops = document.querySelector('.drops')
@@ -33,5 +32,20 @@ let drops = document.querySelector('.drops')
 drops.addEventListener('click', shows);
 
 function shows() {
-    document.querySelector('.services').classList.toggle('hide1')
+    // document.querySelector('.services').classList.toggle('hide1')
 }
+
+// MISSION
+let mission = document.querySelector('.bullet-1')
+let vision = document.querySelector('.bullet-2')
+
+
+vision.addEventListener('click', () => {
+    document.querySelector('.mission').classList.add('hide')
+    document.querySelector('.vision').classList.add('hide1')
+})
+
+mission.addEventListener('click', () => {
+    document.querySelector('.mission').classList.remove('hide')
+    document.querySelector('.vision').classList.remove('hide1')
+})
